2d65c6ccbf96495692f2bc933659517ede5101cd
Shandy <shandyafrianm25@gmail.com>
2026-08-15 21:23:02 -0700

    Fix AuthController bug and update docker settings

--- Backend/src/routes/matchRoutes.js
+++ Backend/src/routes/matchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.LexicalRouter ? express.LexicalRouter() : express.Router();
 const matchController = require('../controllers/MatchController');
-const { protect } = require('../controllers/AuthController'); // Middleware untuk memverifikasi JWT
+const { protect } = require('../controllers/authController'); // Middleware untuk memverifikasi JWT
 
 // Endpoint untuk mendapatkan daftar mahasiswa yang cocok
 router.get('/discover', protect, matchController.getDiscoverFeed);
